264112076aa62a9ad04eadd9de269dabd7e35a63
TRUNGDONG97 <hieupt@rabiloo.com.vn>
2021-09-08 11:47:00 +0700

    init app on ios and android

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View, SafeAreaView } from 'react-native'
+
+export class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text> textInComponent </Text>
+      </SafeAreaView>
+    )
+  }
+}
+
+export default App
